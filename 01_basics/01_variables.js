3e60a9db3d15246510b619fb40fd2eaac066e658
Vijay Korate <121291434+vijaykorate@users.noreply.github.com>
2024-02-17 09:10:24 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 14466
+let accountEmail = "vijay@gmail.com"
+var accountPassword = "12526"
+accountCity = "Mumbai"
+
+//accountId = 2 //not allowed
+
+accountEmail = "ha@hs.com"
+accountPassword = "1222222"
+accountCity = "Bhandup"
+let accountState;
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";  //treat all js code as newer version
+
+let name = "vijay"
+let age = 22
+let isLoggedIn = false 
+
+
+
+/* primitive datatypes
+number
+bigint
+string => ""
+boolean=> true/false
+null +> standalone value
+undefined => 
+symbol => unique
+
+
+objects 
+*/
+console.log(typeof "string");
+console.log(typeof null );
+
+
+
